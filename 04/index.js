5800c1a9b80ab07c350bff4eff16a82c98ac84e5
Edfram-Guerra <edfram.guerra@gmail.com>
2022-09-23 20:37:43 -0300

    Exercicio resolvido

---
+++ 04/index.js
@@ -0,0 +1,39 @@
+
+const usuarios = [
+    {
+        nome: "João",
+        idade: 25,
+    },
+    {
+        nome: "Ana",
+        idade: 18,
+    },
+    {
+        nome: "Beatriz",
+        idade: 15,
+    },
+    {
+        nome: "Carlos",
+        idade: 16,
+    },
+    {
+        nome: "Antonio",
+        idade: 32,
+    },
+];
+
+
+for (let item of usuarios) {
+    if (item.idade > 17) {
+        item.maiorIdade = true;
+    } else {
+        item.maiorIdade = false;
+    }
+
+};
+
+console.log(usuarios);
+
+
+// item.maiorIdade = true; // ou
+// item.maiorIdade = false;
