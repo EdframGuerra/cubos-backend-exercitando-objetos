1701c45ff7e306140ddfddad745452e52f675115
Edfram-Guerra <edfram.guerra@gmail.com>
2022-09-23 23:05:50 -0300

    Exercicio resolvido

---
+++ 09/index.js
@@ -0,0 +1,36 @@
+const participantes = [
+    { nome: "João" },
+    { nome: "Ana" },
+    { nome: "Beatriz" },
+    { nome: "Maria" },
+    { nome: "Ana Clara" },
+    { nome: "Joana" },
+    { nome: "Augusto" },
+    { nome: "Renan" },
+    { nome: "Patricia" },
+    { nome: "Carlos" },
+    { nome: "Renato" },
+    { nome: "José" },
+    { nome: "Roberto" },
+    { nome: "Sara" },
+    { nome: "Junior" },
+    { nome: "Pedro" },
+    { nome: "Vitor" },
+    { nome: "Antonio" },
+]
+
+
+let contador = 0;
+let encontrandoAmigo = "Augusto";
+
+for (let item of participantes) {
+    if (item.nome) {
+        contador++;
+        if (item.nome === encontrandoAmigo) {
+            break
+
+        }
+    }
+
+}
+console.log(`Galera... ${encontrandoAmigo} está na posição ${contador}, corre lá!`);
