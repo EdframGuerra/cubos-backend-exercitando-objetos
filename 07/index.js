c3c066c8a71c3d89d4235731eec6316ba523a1aa
Edfram-Guerra <edfram.guerra@gmail.com>
2022-09-23 21:03:31 -0300

    Exercicio resolvido

---
+++ 07/index.js
@@ -0,0 +1,47 @@
+// adicionar propriedade numero ao objeto
+// :numerar em ordem crescente a propriedade numero:
+
+
+
+const livro = {
+    nome: 'O poder do hábito',
+    capitulos: [
+        {
+            nome: "O loop do hábito",
+        },
+        {
+            nome: "O cérebro ansioso",
+        },
+        {
+            nome: "A regra de ouro da mudança de hábito",
+        },
+        {
+            nome: "Hábitos angulares, ou a balada de Paul O`neill",
+        },
+        {
+            nome: "Starbucks e o hábito do sucesso",
+        },
+        {
+            nome: "O poder de uma crise",
+        },
+        {
+            nome: "Como a target sabe o que você quer antes que você saiba",
+        },
+        {
+            nome: "A Saddleback Church e o boicote aos ônibus de Montgomery",
+        },
+        {
+            nome: "A neorologia do livre-arbítrio",
+        },
+    ]
+
+
+}
+let numero = 0;
+for (let item of livro.capitulos) {
+    numero++;
+    item.numero = numero;
+
+}
+
+console.log(livro)
